861071e7444af628662cc106c6fafb6fb4b69a37
xieyue <68689121+whyxieyue@users.noreply.github.com>
2026-01-28 16:51:26 +0800

    Delete edgeone-simple.sh

--- edgeone-simple.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-wget https://github.com/gohugoio/hugo/releases/download/v0.154.5/hugo_extended_0.154.5_linux-amd64.tar.gz && tar -xzf hugo_extended_0.154.5_linux-amd64.tar.gz && ./hugo --minify
